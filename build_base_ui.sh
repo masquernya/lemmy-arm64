86fec230844b9e11638b41a881c98293bf72c14b
masquernya <127450910+masquernya@users.noreply.github.com>
2023-06-18 15:55:38 -0400

    bug fixes

--- build_base_ui.sh
+++ build_base_ui.sh
@@ -9,4 +9,7 @@ git checkout "$TRANSLATION_COMMIT" || exit 1;
 cd ../;
 git checkout "$LEMMY_VERSION";
 
-docker build . -t arm64v8/ubuntu --tag="masquernya/lemmy:$LEMMY_VERSION-linux-arm64";
+# bug fix: https://github.com/nodejs/docker-node/issues/1912
+sed -i 's/node:alpine/node:20-alpine3.16/g' Dockerfile;
+
+docker build . -t arm64v8/ubuntu --tag="masquernya/lemmy-ui:$LEMMY_VERSION-linux-arm64";
