2439df2c6fbb71c421be6449af273be8c3a410d8
masquernya <127450910+masquernya@users.noreply.github.com>
2023-07-11 23:01:53 -0400

    fork original Dockerfile due to many changes

--- build.sh
+++ build.sh
@@ -12,6 +12,7 @@ git checkout "$TRANSLATION_COMMIT" || exit 1;
 cd ../../../;
 git checkout "$LEMMY_VERSION";
 
+rm -f ./docker/Dockerfile && cp ../Dockerfile ./docker/ || exit 1;
 
 docker build . --build-arg RUST_RELEASE_MODE=release --build-arg CARGO_BUILD_TARGET=aarch64-unknown-linux-gnu --platform linux/arm64 --file ./docker/Dockerfile --tag="masquernya/lemmy:$LEMMY_VERSION-linux-arm64" || exit 1;
 
